059ccb0a25672fc35513061de75fb29232dcd91e
johnnyprochazka <jan.prochazka@heinz-glas.com>
2023-06-07 09:22:14 +0200

    Logging

---
+++ Utils/Logging/MyClass.cs
@@ -0,0 +1,40 @@
+﻿/**********************************************************************************
+ * Filename: MyClass.cs
+ * Author: Jan Procházka
+ * Created: 07-June-2023
+ * Description: Sample class used for logging practice.
+ *
+ * Revision History:
+ *     [1.00.00] [07-June-2023] [Jan Procházka] [Initial]
+ **********************************************************************************/
+
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logging
+{
+    public class MyClass
+    {
+        private static readonly ILogger _logger = Log.ForContext<MyClass>();
+
+        public void DoSomething()
+        {
+            // Log an informational message
+            _logger.Information("Doing something...");
+
+            try
+            {
+                // Perform some operation
+            }
+            catch (Exception ex)
+            {
+                // Log an error message with the exception
+                _logger.Error(ex, "An error occurred while doing something");
+            }
+        }
+    }
+}
--- Utils/Logging/Program.cs
+++ Utils/Logging/Program.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿/**********************************************************************************
+ * Filename: Program.cs
+ * Author: Jan Procházka
+ * Created: 07-June-2023
+ * Description: Sample logging practice.
+ *
+ * Revision History:
+ *     [1.00.00] [07-June-2023] [Jan Procházka] [Initial]
+ **********************************************************************************/
+
+
+using Serilog;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +22,19 @@ namespace Logging
     {
         static void Main(string[] args)
         {
+            // Configure the logger
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug() // Set the minimum log level to Debug
+                .WriteTo.Console() // Send logs to the console
+                .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day) // Send logs to a file
+                .CreateLogger();
+
+
+            var myClass = new MyClass();
+            myClass.DoSomething();
+
+            // Close and flush the logger
+            Log.CloseAndFlush();
         }
     }
 }
